52c5b033b3df72fdb256281b7cc1789b90dd82b8
BrunoB98 <brunob98@outlook.it>
2020-09-11 19:46:54 +0200

    Last update 11/09/20

---
+++ Yaml_single_node/start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+kubectl delete deployment appdb webserver
+kubectl delete service appdb webserver
+kubectl delete pvc appdb-pv-claim ws-pv-claim
+kubectl delete pv local-pv-1 local-pv-2
+kubectl apply -f Local-volumes.yml
+kubectl apply -f Database-pvc.yml
+kubectl apply -f Database-deployment.yml
+kubectl apply -f Database-service.yml
+kubectl apply -f Webserver-pvc.yml
+kubectl apply -f Webserver-service.yml
+kubectl apply -f Webserver-deployment.yml
+minikube service webserver --url
