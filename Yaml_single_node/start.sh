fb7c837fbda9da07c85e6dae024e0575f4522655
BrunoB98 <brunob98@outlook.it>
2020-09-12 12:40:21 +0200

    Update 12:40

--- Yaml_single_node/start.sh
+++ Yaml_single_node/start.sh
@@ -5,11 +5,10 @@ kubectl delete service appdb webserver
 kubectl delete pvc appdb-pv-claim
 kubectl delete pv local-pv-1
 kubectl delete secret mysql-root-password mysql-config
-kubectl apply -f Local-volumes.yml
-kubectl apply -f Database-pvc.yml
+kubectl apply -f Local-volume.yml
+kubectl apply -f PersistentVolumeClaim.yml
 kubectl apply -f Database-deployment.yml
-kubectl apply -f Database-service.yml
-kubectl apply -f Webserver-service.yml
+kubectl apply -f Services.yml
 kubectl apply -f Webserver-deployment.yml
 kubectl create secret generic mysql-config --from-literal=password=0000 --from-literal=username=bruno --from-literal=dbname=shoppingBB
 kubectl create secret generic mysql-root-password --from-literal=password=root
