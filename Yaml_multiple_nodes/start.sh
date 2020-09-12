e3c7a24bab31db2e07af38acdc4eb65eea379f8a
BrunoB98 <brunob98@outlook.it>
2020-09-12 10:02:00 +0200

    Update 10:01

--- Yaml_multiple_nodes/start.sh
+++ Yaml_multiple_nodes/start.sh
@@ -4,11 +4,14 @@ kubectl label node node1 etichetta=sonoEtichetta1
 kubectl label node node2 etichetta=sonoEtichetta2
 kubectl delete deployment appdb webserver
 kubectl delete service appdb webserver
-kubectl delete pvc appdb-pv-claim ws-pv-claim
+kubectl delete pvc appdb-pv-claim
 kubectl delete pv local-pv-1 local-pv-2
+kubectl delete secret mysql-root-password mysql-config
 kubectl apply -f Local-volumes.yml
 kubectl apply -f Database-pvc.yml
 kubectl apply -f Database-deployment.yml
 kubectl apply -f Database-service.yml
 kubectl apply -f Webserver-service.yml
 kubectl apply -f Webserver-deployment.yml
+kubectl create secret generic mysql-config --from-literal=password=0000 --from-literal=username=bruno --from-literal=dbname=shoppingBB
+kubectl create secret generic mysql-root-password --from-literal=password=root
--- Yaml_single_node/start.sh
+++ Yaml_single_node/start.sh
@@ -2,13 +2,15 @@
 
 kubectl delete deployment appdb webserver
 kubectl delete service appdb webserver
-kubectl delete pvc appdb-pv-claim ws-pv-claim
-kubectl delete pv local-pv-1 local-pv-2
+kubectl delete pvc appdb-pv-claim
+kubectl delete pv local-pv-1
+kubectl delete secret mysql-root-password mysql-config
 kubectl apply -f Local-volumes.yml
 kubectl apply -f Database-pvc.yml
 kubectl apply -f Database-deployment.yml
 kubectl apply -f Database-service.yml
-#kubectl apply -f Webserver-pvc.yml
 kubectl apply -f Webserver-service.yml
 kubectl apply -f Webserver-deployment.yml
+kubectl create secret generic mysql-config --from-literal=password=0000 --from-literal=username=bruno --from-literal=dbname=shoppingBB
+kubectl create secret generic mysql-root-password --from-literal=password=root
 minikube service webserver --url
